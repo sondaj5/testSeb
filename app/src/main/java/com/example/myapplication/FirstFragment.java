a09e65c675188e4e5b0493e83e7c7d7cd5067cba
smigalski <smigalski@infrontfinance.com>
2021-04-28 15:30:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/FirstFragment.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class FirstFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        WebView wv = view.findViewById(R.id.webV);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+        wv.loadUrl("https://software.infrontservices.com/seb/SDKsearchTest.html");
+    }
+}
