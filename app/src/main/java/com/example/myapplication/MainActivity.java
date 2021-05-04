5f7120e5ae404430ac2d0d6a673c6099a67445ea
smigalski <smigalski@infrontfinance.com>
2021-05-04 12:25:33 +0200

    update

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,10 +9,15 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.format.DateUtils;
+import android.util.Log;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.io.File;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,9 +45,50 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            clearCache(findViewById(R.id.webV).getContext(), 100);
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    //helper method for clearCache() , recursive
+    //returns number of deleted files
+    static int clearCacheFolder(final File dir, final int numDays) {
+
+        int deletedFiles = 0;
+        if (dir!= null && dir.isDirectory()) {
+            try {
+                for (File child:dir.listFiles()) {
+
+                    //first delete subdirectories recursively
+                    if (child.isDirectory()) {
+                        deletedFiles += clearCacheFolder(child, numDays);
+                    }
+
+                    //then delete the files and subdirectories in this dir
+                    //only empty directories can be deleted, so subdirs have been done first
+                    if (child.lastModified() < new Date().getTime() - numDays * DateUtils.DAY_IN_MILLIS) {
+                        if (child.delete()) {
+                            deletedFiles++;
+                        }
+                    }
+                }
+            }
+            catch(Exception e) {
+                Log.e("TG", String.format("Failed to clean the cache, error %s", e.getMessage()));
+            }
+        }
+        return deletedFiles;
+    }
+
+    /*
+     * Delete the files older than numDays days from the application cache
+     * 0 means all files.
+     */
+    public static void clearCache(final Context context, final int numDays) {
+        Log.i("TG", String.format("Starting cache prune, deleting files older than %d days", numDays));
+        int numDeletedFiles = clearCacheFolder(context.getCacheDir(), numDays);
+        Log.i("TG", String.format("Cache pruning completed, %d files deleted", numDeletedFiles));
+    }
 }
+
